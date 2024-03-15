7a6e191ddde307460e6f8ff184849a26a17d219f
yoarajota <joaovbscontato@gmail.com>
2024-03-15 20:54:19 -0300

    📺 - Backend initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,52 @@
+import express from "express";
+import mongoose from "mongoose";
+import { config } from "dotenv";
+import cors from 'cors'
+import routes from "./routes.js";
+import { Server } from 'socket.io'
+import http from 'http'
+
+config();
+
+// ----- SERVER -----
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use('/api', routes);
+
+app.listen(8000, () => {
+  console.log("✅ - Server");
+});
+
+// ----- SOCKET -----
+try {
+  const server = http.createServer(app);
+  const io = new Server(server);
+
+  io.on('connection', (socket) => {
+  });
+
+  console.log("✅ - SOCKET");
+} catch (error) {
+  console.log("❌ - SOCKET");
+}
+
+// ----- DB -----
+
+const MONGODB_URL = process.env.MONGODB_URL
+
+if (MONGODB_URL) {
+  mongoose.connect(MONGODB_URL, { dbName: "db" });
+
+  const db = mongoose.connection;
+  mongoose.connection.on("error", function (err) {
+    console.log("❌ - DB");
+  });
+
+  db.once("open", function () {
+    console.log("✅ - DB");
+  });
+}
