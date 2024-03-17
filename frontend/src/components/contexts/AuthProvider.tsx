d87678114257bc47da9b46120850d2bb2b7e9067
yoarajota <joaovbscontato@gmail.com>
2024-03-16 22:43:54 -0300

    🎬 - Fixing types, and code syntax

--- frontend/src/components/contexts/AuthProvider.tsx
+++ frontend/src/components/contexts/AuthProvider.tsx
@@ -1,33 +1,36 @@
 import { fakeAuthProvider } from "@/lib/auth";
-import { useState } from "react";
-import { createContext } from "vm";
+import { createContext, useState } from "react";
 
 interface AuthContextType {
-    user: any;
-    signin: (user: string, callback: VoidFunction) => void;
-    signout: (callback: VoidFunction) => void;
+  user: any;
+  signin: (user: string, callback: VoidFunction) => void;
+  signout: (callback: VoidFunction) => void;
 }
 
 export const AuthContext = createContext<AuthContextType>(null!);
 
-export default function AuthProvider({ children }: { children: React.ReactNode }) {
-    let [user, setUser] = useState<any>(null);
+export default function AuthProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  let [user, setUser] = useState<any>(null);
 
-    let signin = (newUser: string, callback: VoidFunction) => {
-        return fakeAuthProvider.signin(() => {
-            setUser(newUser);
-            callback();
-        });
-    };
+  let signin = (newUser: string, callback: VoidFunction) => {
+    return fakeAuthProvider.signin(() => {
+      setUser(newUser);
+      callback();
+    });
+  };
 
-    let signout = (callback: VoidFunction) => {
-        return fakeAuthProvider.signout(() => {
-            setUser(null);
-            callback();
-        });
-    };
+  let signout = (callback: VoidFunction) => {
+    return fakeAuthProvider.signout(() => {
+      setUser(null);
+      callback();
+    });
+  };
 
-    let value = { user, signin, signout };
+  let value = { user, signin, signout };
 
-    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -1,18 +1,20 @@
+import { AuthContext } from "@/components/contexts/AuthProvider";
+import { useContext } from "react";
+
 const fakeAuthProvider = {
-    isAuthenticated: false,
-    signin(callback: VoidFunction) {
-        fakeAuthProvider.isAuthenticated = true;
-        setTimeout(callback, 100); // fake async
-    },
-    signout(callback: VoidFunction) {
-        fakeAuthProvider.isAuthenticated = false;
-        setTimeout(callback, 100);
-    },
+  isAuthenticated: false,
+  signin(callback: VoidFunction) {
+    fakeAuthProvider.isAuthenticated = true;
+    setTimeout(callback, 100); // fake async
+  },
+  signout(callback: VoidFunction) {
+    fakeAuthProvider.isAuthenticated = false;
+    setTimeout(callback, 100);
+  },
 };
 
-
 function useAuth() {
-    return useContext(AuthContext);
+  return useContext(AuthContext);
 }
 
 export { useAuth, fakeAuthProvider };
